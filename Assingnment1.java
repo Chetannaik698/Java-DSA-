2724a459d6ccbbf93a019cb869fb9799b44bfae8
Chetan Naik <chetannaik11145@gmail.com>
2025-01-09 09:55:31 +0530

    Operators in java

---
+++ Assingnment1.java
@@ -0,0 +1,7 @@
+public class Assingnment1 {
+    public static void main(String[] args){
+         int x = 23, y = 33;
+         int exp = (x + y) * 4 / x + (y + y) / x;
+         System.out.println(exp);
+     }
+}
